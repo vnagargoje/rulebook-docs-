ee5674028218f4a726e6054c817d7c871bcccf7c
akashc <akash73.chavan@gmail.com>
2026-04-14 17:45:50 +0530

    fix(app): open home tab on launch

--- apps/app/src/app/(app)/_layout.tsx
+++ apps/app/src/app/(app)/_layout.tsx
@@ -1,32 +1,20 @@
-import { Redirect, SplashScreen, Tabs } from 'expo-router'
+import { SplashScreen, Tabs } from 'expo-router'
 import * as React from 'react'
 import { useCallback, useEffect } from 'react'
 
 import { Home as HomeIcon, Settings as SettingsIcon, Style as StyleIcon } from '@/components/ui/icons'
-import { useAuthStore as useAuth } from '@/features/auth/use-auth-store'
-import { useIsFirstTime } from '@/lib/hooks/use-is-first-time'
 
 export default function TabLayout() {
-    const status = useAuth.use.status()
-    const [isFirstTime] = useIsFirstTime()
     const hideSplash = useCallback(async () => {
         await SplashScreen.hideAsync()
     }, [])
     useEffect(() => {
-        if (status !== 'idle') {
-            const timer = setTimeout(() => {
-                hideSplash()
-            }, 1000)
-            return () => clearTimeout(timer)
-        }
-    }, [hideSplash, status])
+        const timer = setTimeout(() => {
+            hideSplash()
+        }, 1000)
+        return () => clearTimeout(timer)
+    }, [hideSplash])
 
-    if (isFirstTime) {
-        return <Redirect href='/onboarding' />
-    }
-    if (status === 'signOut') {
-        return <Redirect href='/login' />
-    }
     return (
         <Tabs>
             <Tabs.Screen

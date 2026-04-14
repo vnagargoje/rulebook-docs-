5ec8c6dfa9f649e1194c9f981feea557962da82c
ravitejas <ravitejas@consultbop.com>
2026-04-14 19:44:44 +0530

    feat(packages): create aadhar reload captcha handler

---
+++ packages/nestjs/cqrs/src/commands/handlers/kyc/aadhaar-reload-captcha.handler.ts
@@ -0,0 +1,30 @@
+import { ConfigService } from '@nestjs/config'
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+import xior from 'xior'
+import { DeepvueConfig } from '../../../types.js'
+import { AadhaarReloadCaptchaCommand } from '../../impl/kyc/aadhaar-reload-captcha.command.js'
+
+@CommandHandler(AadhaarReloadCaptchaCommand)
+export class AadhaarReloadCaptchaHandler implements ICommandHandler<AadhaarReloadCaptchaCommand> {
+    constructor(private readonly configService: ConfigService) {}
+
+    async execute(command: AadhaarReloadCaptchaCommand) {
+        const { sessionId } = command
+        const config = this.configService.getOrThrow<DeepvueConfig>('deepvue.config')
+        const response = await xior.get(`${config.baseUrl}/ekyc/aadhaar/reload-captcha`, {
+            params: {
+                session_id: sessionId,
+                consent: 'Y',
+                purpose: 'For KYC Purpose',
+            },
+            headers: {
+                'x-api-key': config.apiKey,
+                'client-id': config.clientId,
+                'Content-Type': 'application/json',
+            },
+        })
+        return {
+            captcha: response.data.data.captcha,
+        }
+    }
+}

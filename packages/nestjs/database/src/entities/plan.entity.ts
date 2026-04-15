fc80a577b89a2f5edee7a433f4e6b6e81ab70f15
ravitejas <ravitejas@consultbop.com>
2026-04-15 19:01:49 +0530

    feat(db): create plan entity

---
+++ packages/nestjs/database/src/entities/plan.entity.ts
@@ -0,0 +1,41 @@
+import { Column, Entity } from 'typeorm'
+import { IdTimestamppedEntity } from './id-timestampped.entity.js'
+
+@Entity({ name: 'plans' })
+export class PlanEntity extends IdTimestamppedEntity {
+    @Column('varchar', { length: 150 })
+    name: string
+
+    @Column('text', { nullable: true })
+    description: string
+
+    @Column('int')
+    validityDays: number
+
+    @Column('decimal', { precision: 10, scale: 2 })
+    kmLimit: number
+
+    @Column('decimal', { precision: 10, scale: 2 })
+    price: number
+
+    @Column('decimal', { precision: 10, scale: 2 })
+    deposit: number
+
+    @Column('decimal', { precision: 10, scale: 2, default: 0 })
+    gst: number
+
+    @Column('decimal', { precision: 10, scale: 2, default: 0 })
+    registrationFee: number
+
+    @Column({
+        type: 'decimal',
+        precision: 10,
+        scale: 2,
+        generatedType: 'VIRTUAL',
+        asExpression: 'price + deposit + gst + registrationFee',
+    })
+    totalAmount: number
+
+    @Column('boolean', { default: true })
+    active: boolean
+}

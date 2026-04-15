e035008cba6750e2fc1dfaea8d88424ae519ab6d
ravitejas <ravitejas@consultbop.com>
2026-04-15 18:52:13 +0530

    feat(shared): create user booking status enum

---
+++ packages/shared/shared/src/enums/booking.ts
@@ -0,0 +1,7 @@
+export enum BookingStatus {
+    CREATED = 'created',
+    VEHICLE_ASSIGNED = 'vehicle_assigned',
+    IN_PROGRESS = 'in_progress',
+    COMPLETED = 'completed',
+    CANCELLED = 'cancelled',
+}

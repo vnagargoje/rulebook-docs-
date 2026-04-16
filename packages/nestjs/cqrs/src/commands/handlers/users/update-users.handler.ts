e2a7c38a15a8a0e5316f40c480422850e4da0f30
ravitejas <ravitejas@consultbop.com>
2026-04-16 14:56:06 +0530

    feat(cqrs): add update address support for the update user handler

--- packages/nestjs/cqrs/src/commands/handlers/users/update-users.handler.ts
+++ packages/nestjs/cqrs/src/commands/handlers/users/update-users.handler.ts
@@ -1,7 +1,7 @@
 import { NotFoundException } from '@nestjs/common'
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
 import { InjectEntityManager } from '@nestjs/typeorm'
-import { RoleEntity, UserEntity } from '@yugo/nestjs-database/entities'
+import { AddressEntity, RoleEntity, UserEntity } from '@yugo/nestjs-database/entities'
 import { EntityManager } from 'typeorm'
 import { UpdateUserCommand } from '../../impl/users/update-users.command.js'
 
@@ -15,7 +15,7 @@ export class UpdateUserHandler implements ICommandHandler<UpdateUserCommand> {
         return this.manager.transaction(async (manager) => {
             const user = await manager.findOne(UserEntity, {
                 where: { id: userId },
-                relations: { roles: true },
+                relations: { roles: true, addresses: { city: { state: true } } },
             })
 
             if (!user) {
@@ -33,10 +33,37 @@ export class UpdateUserHandler implements ICommandHandler<UpdateUserCommand> {
                 user.roles = [role]
             }
 
-            const { role, ...updateData } = body
+            if (body.address) {
+                const existingAddress = user.addresses?.[0]
+                if (existingAddress) {
+                    Object.assign(existingAddress, {
+                        lineOne: body.address.lineOne ?? existingAddress.lineOne,
+                        lineTwo: body.address.lineTwo ?? existingAddress.lineTwo,
+                        pincode: body.address.pincode ?? existingAddress.pincode,
+                        cityId: body.address.cityId ?? existingAddress.cityId,
+                    })
+                    await manager.save(AddressEntity, existingAddress)
+                } else {
+                    const address = manager.create(AddressEntity, {
+                        lineOne: body.address.lineOne,
+                        lineTwo: body.address.lineTwo,
+                        pincode: body.address.pincode,
+                        cityId: body.address.cityId,
+                        userId: user.id,
+                    })
+                    await manager.save(address)
+                }
+            }
+
+            const { role, address, ...updateData } = body
             Object.assign(user, updateData)
 
-            return manager.save(user)
+            await manager.save(user)
+
+            return manager.findOne(UserEntity, {
+                where: { id: userId },
+                relations: { roles: true, addresses: { city: { state: true } } },
+            })
         })
     }
 }

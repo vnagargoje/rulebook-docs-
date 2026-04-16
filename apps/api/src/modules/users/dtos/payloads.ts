f6e34afa1a04f328e6433922e939a5d554ee4bca
ravitejas <ravitejas@consultbop.com>
2026-04-16 14:57:04 +0530

    feat(api): create address payload dto

--- apps/api/src/modules/users/dtos/payloads.ts
+++ apps/api/src/modules/users/dtos/payloads.ts
@@ -1,6 +1,13 @@
 import { Type } from '@sinclair/typebox';
 import { Gender, Roles } from '@yugo/shared';
 
+export const AddressPayload = Type.Object({
+    lineOne: Type.String(),
+    lineTwo: Type.Optional(Type.String()),
+    pincode: Type.String(),
+    cityId: Type.Optional(Type.String()),
+});
+
 export const CreateUserPayload = Type.Object({
     email: Type.Optional(Type.String({ format: 'email' })),
     mobilenumber: Type.String(),
@@ -10,6 +17,7 @@ export const CreateUserPayload = Type.Object({
     role: Type.Optional(Type.Enum(Roles)),
     properties: Type.Optional(Type.Any()),
     dateOfBirth: Type.Optional(Type.String({ format: 'date' })),
+    address: Type.Optional(AddressPayload),
 });
 
 export const UpdateUserPayload = Type.Partial(CreateUserPayload);

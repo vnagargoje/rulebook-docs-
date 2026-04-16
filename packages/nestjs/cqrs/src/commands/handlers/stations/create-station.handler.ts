485ae7c2e627f98dbf1227c62f1d392f8c99169e
ravitejas <ravitejas@consultbop.com>
2026-04-16 16:36:31 +0530

    feat(cqrs): create station command handler

---
+++ packages/nestjs/cqrs/src/commands/handlers/stations/create-station.handler.ts
@@ -0,0 +1,46 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+import { InjectEntityManager } from '@nestjs/typeorm'
+import { AddressEntity, StationEntity, HubStationEntity, SwapStationEntity } from '@yugo/nestjs-database/entities'
+import { CreateStationCommand } from 'src/commands/impl/stations/create-station.command.js'
+import { EntityManager } from 'typeorm'
+import { StationType } from '@yugo/shared'
+
+@CommandHandler(CreateStationCommand)
+export class CreateStationHandler implements ICommandHandler<CreateStationCommand> {
+    constructor(@InjectEntityManager() private readonly manager: EntityManager) {}
+
+    async execute(command: CreateStationCommand) {
+        const { payload } = command
+
+        return this.manager.transaction(async (manager) => {
+            const entityClass = payload.type === StationType.HUB_STATION ? HubStationEntity : SwapStationEntity
+            const station = manager.create(entityClass, {
+                name: payload.name,
+                type: payload.type,
+                longitude: payload.longitude,
+                latitude: payload.latitude,
+                active: payload.active,
+                managerId: payload.managerId,
+            })
+
+            await manager.save(station)
+
+            if (payload.address) {
+                const address = manager.create(AddressEntity, {
+                    lineOne: payload.address.lineOne,
+                    lineTwo: payload.address.lineTwo,
+                    pincode: payload.address.pincode,
+                    cityId: payload.address.cityId,
+                })
+                await manager.save(address)
+                station.address = address
+                await manager.save(station)
+            }
+
+            return manager.findOne(StationEntity, {
+                where: { id: station.id },
+                relations: { address: true },
+            })
+        })
+    }
+}

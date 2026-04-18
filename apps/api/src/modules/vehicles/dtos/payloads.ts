4282d783a61ebdc8df7c4e0710e5a46a6812e402
NalagamdinniRaju <nalagamdinniraju@gmail.com>
2026-04-18 13:58:59 +0530

    feat(api): create vehicle payload dtos

---
+++ apps/api/src/modules/vehicles/dtos/payloads.ts
@@ -0,0 +1,18 @@
+import { Type } from '@sinclair/typebox';
+
+export const VehiclePropertiesPayload = Type.Object({
+    brand: Type.Optional(Type.String()),
+    model: Type.Optional(Type.String()),
+    insuranceExpiry: Type.Optional(Type.String()),
+});
+
+export const CreateVehiclePayload = Type.Object({
+    vehicleNumber: Type.Optional(Type.String()),
+    rcNumber: Type.Optional(Type.String()),
+    chassisNumber: Type.Optional(Type.String()),
+    gpsId: Type.Optional(Type.String()),
+    properties: Type.Optional(VehiclePropertiesPayload),
+    stationId: Type.Optional(Type.String()),
+});
+
+export const UpdateVehiclePayload = Type.Partial(CreateVehiclePayload);

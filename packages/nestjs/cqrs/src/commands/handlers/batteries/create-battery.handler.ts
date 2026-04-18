4baaa1143b4a667ad6ad665044373a9d5feb5181
NalagamdinniRaju <nalagamdinniraju@gmail.com>
2026-04-18 14:02:52 +0530

    feat(cqrs): add create battery command handler

---
+++ packages/nestjs/cqrs/src/commands/handlers/batteries/create-battery.handler.ts
@@ -0,0 +1,30 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+import { InjectEntityManager } from '@nestjs/typeorm'
+import { BatteryEntity } from '@yugo/nestjs-database/entities'
+import { CreateBatteryCommand } from 'src/commands/impl/batteries/create-battery.command.js'
+import { EntityManager } from 'typeorm'
+
+@CommandHandler(CreateBatteryCommand)
+export class CreateBatteryHandler implements ICommandHandler<CreateBatteryCommand> {
+    constructor(@InjectEntityManager() private readonly manager: EntityManager) {}
+
+    async execute(command: CreateBatteryCommand) {
+        const { payload } = command
+
+        return this.manager.transaction(async (manager) => {
+            const battery = manager.create(BatteryEntity, {
+                batteryId: payload.batteryId,
+                gpsId: payload.gpsId,
+                properties: payload.properties,
+                stationId: payload.stationId,
+            })
+
+            await manager.save(battery)
+
+            return manager.findOne(BatteryEntity, {
+                where: { id: battery.id },
+                relations: { station: true },
+            })
+        })
+    }
+}

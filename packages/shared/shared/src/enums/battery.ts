5ee23a9dd0e3c5daf4e46442c7cf62a3c96102b3
ravitejas <ravitejas@consultbop.com>
2026-04-20 15:46:03 +0530

    feat(shared): create battery status enums

---
+++ packages/shared/shared/src/enums/battery.ts
@@ -0,0 +1,13 @@
+export enum BatteryStatus {
+    AVAILABLE = 'available',
+    CHARGED = 'charged',
+    CHARGING = 'charging',
+    DRAINED = 'drained',
+    IN_TRANSIT = 'in_transit',
+    IN_USE = 'in_use',
+}
+
+export enum BatteryMovementStatus {
+    IN_TRANSIT = 'in_transit',
+    DELIVERED = 'delivered',
+}

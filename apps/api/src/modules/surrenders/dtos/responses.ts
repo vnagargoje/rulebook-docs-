1bd67120ded9bcd3021dd17ed6b7270d996c234e
ravitejas <ravitejas@consultbop.com>
2026-04-21 16:48:39 +0530

    feat(api): create surrender vehicle response dto

--- apps/api/src/modules/surrenders/dtos/responses.ts
+++ apps/api/src/modules/surrenders/dtos/responses.ts
@@ -7,3 +7,18 @@ export const VehicleSurrenderDetailsResponse = Type.Object({
     rtoPenalty: Type.Number(),
     refundAmount: Type.Number(),
 });
+
+export const SurrenderVehicleResponse = Type.Object({
+    id: Type.String(),
+    bookingId: Type.String(),
+    userPlanId: Type.String(),
+    vehicleId: Type.String(),
+    vehicle: Type.Object({
+        id: Type.String(),
+        vehicleNumber: Type.String(),
+    }),
+    penalty: Type.Number(),
+    miscCharges: Type.Number(),
+    refundAmount: Type.Number(),
+    notes: Type.Union([Type.String(), Type.Null()]),
+});

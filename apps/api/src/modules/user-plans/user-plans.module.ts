9fa18ba9d45ad9f7a151cbfd5e1a20c5ae57ce66
ravitejas <ravitejas@consultbop.com>
2026-04-23 14:33:11 +0530

    feat(api): add webhook controller in user plans module

--- apps/api/src/modules/user-plans/user-plans.module.ts
+++ apps/api/src/modules/user-plans/user-plans.module.ts
@@ -3,17 +3,20 @@ import {
     ApplyTopUpHandler,
     PurchasePlanHandler,
     GetUserPlanByQrHandler,
+    VerifyPaymentHandler,
 } from '@yugo/cqrs';
 import { V1UserPlansController } from './controllers/v1/user-plans.controller';
+import { V1WebhooksController } from './controllers/v1/webhooks.controller';
 
 const Handlers = [
     PurchasePlanHandler,
     ApplyTopUpHandler,
     GetUserPlanByQrHandler,
+    VerifyPaymentHandler,
 ];
 
 @Module({
-    controllers: [V1UserPlansController],
+    controllers: [V1UserPlansController, V1WebhooksController],
     providers: [...Handlers],
 })
 export class UserPlansModule {}

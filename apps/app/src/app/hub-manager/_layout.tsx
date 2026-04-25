732eb3b705c9e8df5251e7dcb34b074b7b346fac
rajun <nalagamdinniraju@gmail.com>
2026-04-25 21:35:31 +0530

    feat(app): update Hub Manager layout to include screen options and styling

--- apps/app/src/app/hub-manager/_layout.tsx
+++ apps/app/src/app/hub-manager/_layout.tsx
@@ -1,11 +1,17 @@
 import { Stack } from 'expo-router'
 
+import { colors } from '@/components/ui'
+
 export default function HubManagerLayout() {
     return (
         <Stack
             screenOptions={{
                 headerShown: false,
-            }}
-        />
+                contentStyle: { backgroundColor: colors.white },
+            }}>
+            <Stack.Screen name='(tabs)' />
+            <Stack.Screen name='battery-inward' />
+            <Stack.Screen name='battery-outward' />
+        </Stack>
     )
 }

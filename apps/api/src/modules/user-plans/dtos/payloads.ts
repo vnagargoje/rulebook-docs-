2ee581eed6032972ab2f5cb03ff79e068a073b1e
rajun <nalagamdinniraju@gmail.com>
2026-04-29 16:08:47 +0530

    feat(api): add payloads for PurchaseTopUp and VerifyTopUpPayment

--- apps/api/src/modules/user-plans/dtos/payloads.ts
+++ apps/api/src/modules/user-plans/dtos/payloads.ts
@@ -14,3 +14,17 @@ export const ApplyTopUpPayload = Type.Object({
     topUpId: Type.String(),
     userPlanId: Type.String(),
 });
+
+export const PurchaseTopUpPayload = Type.Object({
+    topUpId: Type.String(),
+    userPlanId: Type.String(),
+});
+
+export const VerifyTopUpPaymentPayload = Type.Object({
+    razorpayOrderId: Type.String(),
+    razorpayPaymentId: Type.String(),
+    razorpaySignature: Type.String(),
+    topUpId: Type.String(),
+    userPlanId: Type.String(),
+});
+

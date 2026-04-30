2ab1d6a91c4ef4bf0b46df4a43deddee7256d2ab
ravitejas <ravitejas@consultbop.com>
2026-04-30 17:01:04 +0530

    chore: barrel export mooving config file

--- apps/henchmen/src/config/index.ts
+++ apps/henchmen/src/config/index.ts
@@ -2,3 +2,4 @@ export * from './database.config.js';
 export * from './redis.config.js';
 export * from './inngest.config.js';
 export * from './logger.config.js';
+export * from './mooving.config.js';

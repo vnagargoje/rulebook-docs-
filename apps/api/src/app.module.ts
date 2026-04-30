d54bfe0c8de19ba2f7cdbc9fe80da0b6bf2eda5c
ravitejas <ravitejas@consultbop.com>
2026-04-30 12:22:10 +0530

    feat(api): import inngest module in root api module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -16,6 +16,7 @@ import {
     razorpayConfig,
     s3ClientConfig,
     s3BucketConfig,
+    inngestConfig,
 } from './config';
 import { AppAuthGuard } from './guards/app.guard.js';
 import { TypeboxSerializerInterceptor } from './interceptors/typebox-serializer.interceptor.js';
@@ -36,6 +37,7 @@ import { BatteriesModule } from './modules/batteries/batteries.module';
 import { BatteryTransportsModule } from './modules/battery-transports/battery-transports.module';
 import { BatterySwapsModule } from './modules/battery-swaps/battery-swaps.module';
 import { SurrendersModule } from './modules/surrenders/surrenders.module';
+import { NestjsInngestModule } from '@yugo/nestjs-inngest';
 
 @Module({
     imports: [
@@ -51,6 +53,7 @@ import { SurrendersModule } from './modules/surrenders/surrenders.module';
                 razorpayConfig,
                 s3ClientConfig,
                 s3BucketConfig,
+                inngestConfig,
             ],
         }),
         LoggerModule.forRootAsync({
@@ -78,6 +81,13 @@ import { SurrendersModule } from './modules/surrenders/surrenders.module';
             inject: [ConfigService],
             useFactory(configService: ConfigService) {
                 return configService.getOrThrow('jwt.config');
+            },
+        }),
+        NestjsInngestModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            async useFactory(configService: ConfigService) {
+                return configService.getOrThrow('inngest.config');
             },
         }),
         PassportModule.register({

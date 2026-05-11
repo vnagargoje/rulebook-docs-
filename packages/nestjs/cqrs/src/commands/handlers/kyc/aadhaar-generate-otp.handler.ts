0bf9123213f886a517279db79fdeccfea4dcedf3
kalpeshp <kalpesh200071@gmail.com>
2026-05-11 18:23:45 +0530

    refactor(cqrs): while generating otp for aadhar it first creates kyc record in the db

--- packages/nestjs/cqrs/src/commands/handlers/kyc/aadhaar-generate-otp.handler.ts
+++ packages/nestjs/cqrs/src/commands/handlers/kyc/aadhaar-generate-otp.handler.ts
@@ -20,6 +20,25 @@ export class AadhaarGenerateOtpHandler implements ICommandHandler<AadhaarGenerat
         const manager = this.datasource.manager
         const config = this.configService.getOrThrow<DeepvueConfig>('deepvue.config')
 
+        await manager.transaction(async (manager) => {
+            let kyc = await manager.findOne(UserKycEntity, {
+                where: { userId, type: KycDocumentType.AADHAR },
+            })
+
+            if (!kyc) {
+                kyc = manager.create(UserKycEntity, {
+                    userId,
+                    type: KycDocumentType.AADHAR,
+                })
+            }
+
+            kyc.documentId = payload.aadhaarNumber
+            kyc.status = KycStatus.PENDING
+            kyc.notes = 'Initiated'
+
+            await manager.save(kyc)
+        })
+
         const response = await xior.post(
             `${config.baseUrl}/ekyc/aadhaar/generate-otp`,
             {}, // Empty body

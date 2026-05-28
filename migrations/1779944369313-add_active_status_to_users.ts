1e9ae9bbbdd78fbed0e81c4ca1d3ad983705abb4
akashc <akash73.chavan@gmail.com>
2026-05-28 12:47:01 +0530

    feat(migrations): add migration to add active status column to users table

---
+++ migrations/1779944369313-add_active_status_to_users.ts
@@ -0,0 +1,13 @@
+import type { MigrationInterface, QueryRunner } from "typeorm";
+
+export class addActiveStatusToUsers1779944369313 implements MigrationInterface {
+    name = 'addActiveStatusToUsers1779944369313'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`users\` ADD \`active\` tinyint NOT NULL DEFAULT 1`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`users\` DROP COLUMN \`active\``);
+    }
+}

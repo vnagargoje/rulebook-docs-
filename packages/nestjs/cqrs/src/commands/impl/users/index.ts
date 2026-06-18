1e219c15b9c6234516d78588438061595ec984a4
kalpeshp <kalpesh200071@gmail.com>
2026-06-18 11:49:50 +0530

    feat(cqrs): export register device token command from users entry point to expose the command to callers of cqrs

--- packages/nestjs/cqrs/src/commands/impl/users/index.ts
+++ packages/nestjs/cqrs/src/commands/impl/users/index.ts
@@ -1,3 +1,4 @@
 export * from './create-users.command.js'
 export * from './update-users.command.js'
 export * from './update-user-addresses.command.js'
+export * from './register-device-token.command.js'
